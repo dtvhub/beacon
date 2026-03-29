45f8305d4a3e561ebc42063e655c2ea80e046af9
dtvhub <100465368+dtvhub@users.noreply.github.com>
2026-03-28 21:37:04 -0400

    Update map.js

--- map/js/map.js
+++ map/js/map.js
@@ -5,8 +5,10 @@
 // Create the map centered on Lexington
 const map = L.map("map").setView([38.0464, -84.4970], 12);
 
-// Add the dark, pretty tile layer
+// Add a guaranteed-working dark tile layer
 L.tileLayer("https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}.png", {
   maxZoom: 20,
   attribution: "&copy; Stadia Maps, OpenMapTiles & OpenStreetMap contributors"
 }).addTo(map);
+
+console.log("MAP JS LOADED");
