b0cd7daa95ecf8038f8974818051302225db6c10
dtvhub <100465368+dtvhub@users.noreply.github.com>
2026-04-11 12:31:32 -0400

    Update cameras.js

--- map/js/cameras.js
+++ map/js/cameras.js
@@ -1,1 +1,8 @@
-
+L.geoJSON(cameraLayer, {
+  onEachFeature: (feature, layer) => {
+    layer.bindPopup(`
+      <strong>${feature.properties.name}</strong><br>
+      <a href="${feature.properties.url}" target="_blank">Open Camera</a>
+    `);
+  }
+}).addTo(map);
